50b1b03e340c678aa64815f80284edf011c11180
Hannah B <hannahb@Learnerss-MacBook-Pro.local>
2016-11-29 17:21:38 -0800

    Add mean and edited multiply

---
+++ src/lib/mean.js
@@ -0,0 +1,3 @@
+const (array) => {
+      return baseMean (array, identity);
+    };
---
+++ src/lib/multiply.js
@@ -0,0 +1,7 @@
+const multiply = (multiplier, multiplicand) => {
+     return multiplier * multiplicand;
+   };
+
+   const multiply = (3,5)
+
+   console.log();
