8791547e50a50a58361b9074b1136f850a3e0f93
Hannah B <hannahb@Learnerss-MacBook-Pro.local>
2016-11-30 10:32:39 -0800

    branch

---
+++ test/add-test.js
@@ -0,0 +1,11 @@
+import {add} from '../src/lib/add'
+const {expect} = require('chai');
+
+describe("add", () => {
+  it('add numbers together', () => {
+    expect(add(2, 3)).to.equal(5)
+  })
+  it('should return undefined if no arguments are given'() => {
+    expect(add()).to.equal(undefined)
+  })
+})
