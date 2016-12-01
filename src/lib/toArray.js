57dcec359964c432bf3b54f7707f9ce2634d1521
Breyana Scales <Breyana@anasauce1.local>
2016-12-01 14:52:23 -0800

    adding in fix for toArray

--- src/lib/toArray.js
+++ src/lib/toArray.js
@@ -1,8 +1,12 @@
 function toArray(input) {
+  //console.log("this is the input: " + input)
   var convertedArray = []
-  for (i=0; i > input.length;i++) {
+  for ( i=0; i < input.length; i++ ) {
+    //console.log("this is the input: " + input)
     convertedArray.push(input[i])
   }
+  //console.log("this is the converted array " + convertedArray)
+  return convertedArray
 }
 
 module.exports = {toArray};
