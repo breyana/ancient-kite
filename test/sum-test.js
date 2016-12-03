099c1a2d276208e82530e38d067701ab0fa1b160
HJBowers <hjbowers@gmail.com>
2016-12-02 16:35:05 -0800

    add sum

---
+++ test/sum-test.js
@@ -0,0 +1,8 @@
+const {sum} = require('../src/lib/sum');
+const {expect} = require('chai');
+
+describe('Sum', function() {
+  it('should give the sum of all values', function() {
+    expect(sum(2, 3,6,20,1,8)).to.equal(40)
+  })
+})
