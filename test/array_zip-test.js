9a939fb1a29877955f63dbdf76786065f1d4c1a5
Breyana Scales <Breyana@anasauce1.local>
2016-12-02 16:18:35 -0800

    changed letters to strings to fix reference error for the missing letter funtions

--- test/array_zip-test.js
+++ test/array_zip-test.js
@@ -3,6 +3,6 @@ const {expect} = require('chai')
 
 describe('Zip', function() {
   it('Should create separate arrays elements of the given arrays depending on their position.', function() {
-    expect(zip( [1,2,3,4],[a,b,c,d],[5,6,7,8] )).to.eql( [[1,a,5],[2,b,6],[3,c,7],[4,d,8]] )
+    expect(zip( [1,2,3,4],["a","b","c","d"],[5,6,7,8] )).to.eql( [[1,"a",5],[2,"b",6],[3,"c",7],[4,"d",8]] )
   })
 })
